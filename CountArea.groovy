20ec4d0043b83b16b8433a76c9a601bfe986282c
kelbakkouri <kelbakkouri@ufl.edu>
2025-02-20 01:08:29 -0500

    Meant to be a script for counting area

---
+++ CountArea.groovy
@@ -0,0 +1,5 @@
+setImageType('FLUORESCENCE');
+createFullImageAnnotation(true)
+createAnnotationsFromPixelClassifier("Test", 5.0, 0.0, "INCLUDE_IGNORED")
+clearSelectedObjects(true);
+clearSelectedObjects();
