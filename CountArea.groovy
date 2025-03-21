48ac37bd0f523cea56f72e28ff5dda8675784d87
Kadin El Bakkouri <kelbakkouri@ufl.edu>
2025-03-21 00:01:28 -0400

    Delete CountArea.groovy
    deleting junk

--- CountArea.groovy
+++
@@ -1,5 +0,0 @@
-setImageType('FLUORESCENCE');
-createFullImageAnnotation(true)
-createAnnotationsFromPixelClassifier("Test", 5.0, 0.0, "INCLUDE_IGNORED")
-clearSelectedObjects(true);
-clearSelectedObjects();
