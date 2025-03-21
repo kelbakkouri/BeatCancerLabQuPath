00bae86b0fa33833f134e4b96d0469092fc046f5
Kadin El Bakkouri <kelbakkouri@ufl.edu>
2025-03-21 00:01:14 -0400

    Delete Run image counting analysis.groovy
    deleting junk

--- Run image counting analysis.groovy
+++
@@ -1,5 +0,0 @@
-setImageType('FLUORESCENCE'); 
-
-createFullImageAnnotation(true) 
-
-runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage":"Channel 1","requestedPixelSizeMicrons":0.5,"backgroundRadiusMicrons":8.0,"backgroundByReconstruction":true,"medianRadiusMicrons":0.0,"sigmaMicrons":1.5,"minAreaMicrons":10.0,"maxAreaMicrons":400.0,"threshold":300.0,"watershedPostProcess":false,"cellExpansionMicrons":5.0,"includeNuclei":false,"smoothBoundaries":true,"makeMeasurements":true}') 
